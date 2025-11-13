33fb8ac4b49c2f3e45d6ab14f1c5460d206d1dae
Vitalii Dutov <dutov.vit.vic@gmail.com>
2025-11-13 13:48:20 +0200

    added material dialog

--- src/app/features/tasks-list/tasks-list.ts
+++ src/app/features/tasks-list/tasks-list.ts
@@ -1,18 +1,37 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { Task, TASK_STATUS } from '../../models/task.model';
 import { TaskCard } from '../task-card/task-card';
 import { TasksService } from '../../services/tasks-service';
 import { TaskForm } from '../task-form/task-form';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-tasks-list',
-  imports: [TaskCard, TaskForm, MatTabsModule],
+  imports: [TaskCard, MatTabsModule, MatButtonModule],
   templateUrl: './tasks-list.html',
   styleUrl: './tasks-list.scss',
 })
 export class TasksList {
   private tasksService = inject(TasksService);
+
+  readonly dialog = inject(MatDialog);
+
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(DialogAnimationsExampleDialog, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
 
   constructor() {
     this.tasksService.reloadTasks();
@@ -33,4 +52,14 @@ export class TasksList {
   addTask(task: Task) {
     this.tasksService.addTask(task);
   }
+}
+
+@Component({
+  selector: 'dialog-animations-example-dialog',
+  templateUrl: 'dialog-animations-example-dialog.html',
+  imports: [MatButtonModule, MatDialogClose, MatDialogTitle, MatDialogActions, MatDialogContent, TaskForm],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DialogAnimationsExampleDialog {
+  readonly dialogRef = inject(MatDialogRef<DialogAnimationsExampleDialog>);
 }
